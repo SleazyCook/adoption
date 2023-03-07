42e57e6cdff9203ec00d7e578bafa0583de098da
Andrew Cook <andrewjcook93@gmail.com>
2023-03-06 23:52:51 -0600

    start pets cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,13 @@ import { GiSittingDog, GiCat, GiRabbit, GiHorseHead } from 'react-icons/gi'
 import { MdMedicalServices, MdFamilyRestroom, MdVolunteerActivism } from 'react-icons/md'
 import { FaHandsHelping, FaHome } from 'react-icons/fa'
 import { BsBriefcaseFill } from 'react-icons/bs'
+import { TbWeight } from 'react-icons/tb'
+import { AiOutlineCalendar } from 'react-icons/ai'
+import { TbPaw } from 'react-icons/tb'
 
 import VideoEdit from './img/video-edit.mp4'
+import Dog02 from './img/dog-02.png'
+import Dog03 from './img/dog-03.jpg'
 
 function Home() {
   return (
@@ -30,15 +35,15 @@ function Home() {
       <section className="features">
         <div className="feature">
           < MdMedicalServices  className="feature__icon"/>
-          <h3 className="heading-3">Animal Rescue </h3>
+          <h3 className="heading-3 heading-3--feature">Animal Rescue </h3>
           <p className="feature__text">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.
           </p>
         </div>
 
         <div className="feature">
-          < MdFamilyRestroom  className="feature__icon"/>
-          <h3 className="heading-3">Adoption </h3>
+          < MdFamilyRestroom  className="feature__icon "/>
+          <h3 className="heading-3 heading-3--feature">Adoption </h3>
           <p className="feature__text">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.
           </p>
@@ -46,7 +51,7 @@ function Home() {
 
         <div className="feature">
           < FaHandsHelping  className="feature__icon"/>
-          <h3 className="heading-3">Fostering </h3>
+          <h3 className="heading-3 heading-3--feature">Fostering </h3>
           <p className="feature__text">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.
           </p>
@@ -54,7 +59,7 @@ function Home() {
 
         <div className="feature">
           < FaHome  className="feature__icon"/>
-          <h3 className="heading-3">Rehome </h3>
+          <h3 className="heading-3 heading-3--feature">Rehome </h3>
           <p className="feature__text">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.
           </p>
@@ -62,16 +67,16 @@ function Home() {
 
         <div className="feature">
           < MdVolunteerActivism  className="feature__icon"/>
-          <h3 className="heading-3">Volunteering </h3>
+          <h3 className="heading-3 heading-3--feature">Volunteering </h3>
           <p className="feature__text">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.
           </p>
         </div>
 
         <div className="feature">
-          < BsBriefcaseFill
-  className="feature__icon"/>
-          <h3 className="heading-3">Careers </h3>
+          < BsBriefcaseFill 
+            className="feature__icon"/>
+          <h3 className="heading-3 heading-3--feature">Careers </h3>
           <p className="feature__text">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.
           </p>
@@ -85,13 +90,54 @@ function Home() {
       </div>
 
       <div className="story__content">
-        <h3 className="heading-3">Happy Families</h3>
-        <h4 className="heading-4">"Our home has been brighter ever since he came home"</h4>
-        <p className="story__text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus. Quidem consequatur harum volupta!
-        </p>
-        <button className="btn">Find your pet</button>
+        <h3 className="heading-3 heading-3--story">Happy Families</h3>
+        <p className="story__text">"Our home has been happier ever since he came home with us"</p>
+        {/* <button className="btn">Find your pet</button> */}
+        <img className="story__img" src={Dog02} />
       </div>
+
+      <section className="pets">
+        <div className="pet">
+          <img className="pet__image" src={Dog03}/>
+          <div className="pet__name">
+            Skittles</div>
+            <div className="pet__detail pet__detail--1">
+              <TbPaw />Chihuahua</div>
+            <div className="pet__detail pet__detail--2">
+              <TbWeight /> 7 lbs</div>
+            <div className="pet__detail pet__detail--3">
+              <AiOutlineCalendar /> 18 mo</div>
+              <button className="pet__button">Adopt me</button>
+          </div>
+
+          <div className="pet">
+          <img className="pet__image" src={Dog03}/>
+          <div className="pet__name">
+            Skittles</div>
+            <div className="pet__detail pet__detail--1">
+              <TbPaw />Chihuahua</div>
+            <div className="pet__detail pet__detail--2">
+              <TbWeight /> 7 lbs</div>
+            <div className="pet__detail pet__detail--3">
+              <AiOutlineCalendar /> 18 mo</div>
+              <button className="pet__button">Adopt me</button>
+          </div>
+
+          <div className="pet">
+          <img className="pet__image" src={Dog03}/>
+          <div className="pet__name">
+            Skittles</div>
+            <div className="pet__detail pet__detail--1">
+              <TbPaw />Pug</div>
+            <div className="pet__detail pet__detail--2">
+              <TbWeight /> 7 lbs</div>
+            <div className="pet__detail pet__detail--3">
+              <AiOutlineCalendar /> 18 mo</div>
+            <button className="pet__button">Adopt me</button>
+          </div>
+
+
+      </section>
 
 
     </div>
